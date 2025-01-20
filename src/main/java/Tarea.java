b25ae493963a2e9a0d0f985a031d907099c886cf
Dani28MR <dmrdaniel2804@gmail.com>
2025-01-20 10:21:00 +0100

    actualizar Tarea

--- src/main/java/Tarea.java
+++ src/main/java/Tarea.java
@@ -1,4 +1,15 @@
+import java.time.LocalDate;
+
 public class Tarea {
     private String nombre;
+    private String description;
+    private boolean completada;
+    private LocalDate fechaTarea;
 
+    public Tarea(String nombre, String description, boolean completada, LocalDate fechaTarea) {
+        this.nombre = nombre;
+        this.description = description;
+        this.completada = completada;
+        this.fechaTarea = fechaTarea;
+    }
 }

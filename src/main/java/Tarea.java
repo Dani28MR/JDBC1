afe2bbbc9747b7c74cff7bdb67410ee17c731b3b
Dani28MR <dmrdaniel2804@gmail.com>
2025-01-20 10:54:51 +0100

    geters and setters tareas

--- src/main/java/Tarea.java
+++ src/main/java/Tarea.java
@@ -5,11 +5,67 @@ public class Tarea {
     private String description;
     private boolean completada;
     private LocalDate fechaTarea;
+    private int nivelImportancia;
 
-    public Tarea(String nombre, String description, boolean completada, LocalDate fechaTarea) {
+    public Tarea(String nombre, String description, boolean completada, LocalDate fechaTarea, int nivelImportancia) {
         this.nombre = nombre;
         this.description = description;
         this.completada = completada;
         this.fechaTarea = fechaTarea;
+        this.nivelImportancia = nivelImportancia;
     }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isCompletada() {
+        return this.completada;
+    }
+
+    public void setCompletada(boolean completada) {
+        this.completada = completada;
+    }
+
+    public LocalDate getFechaTarea() {
+        return this.fechaTarea;
+    }
+
+    public void setFechaTarea(LocalDate fechaTarea) {
+        this.fechaTarea = fechaTarea;
+    }
+
+    public int getNivelImportancia() {
+        return this.nivelImportancia;
+    }
+
+    public void setNivelImportancia(int nivelImportancia) {
+        this.nivelImportancia = nivelImportancia;
+    }
+
+    @Override
+    public String toString() {
+        return "Tarea{" +
+                "nombre='" + nombre + '\'' +
+                ", description='" + description + '\'' +
+                ", completada=" + completada +
+                ", fechaTarea=" + fechaTarea +
+                ", nivelImportancia=" + nivelImportancia +
+                '}';
+    }
+
+
+
 }

7853e1cb11150a3e8e96213458f7b181b79e4735
Dani28MR <dmrdaniel2804@gmail.com>
2025-01-21 14:10:15 +0100

    change in Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
 
+public class Main {
+    public static void main(String[] args) throws UnsupportedEncodingException {
+        System.setOut(new PrintStream(System.out,true,"UTF-8"));
+        System.out.println("Iniciando aplicación...");
     }
 }

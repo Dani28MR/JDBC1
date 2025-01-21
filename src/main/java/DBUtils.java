8abdba8897f1392b73a0a56ab301fb61cdb90eaf
Dani28MR <dmrdaniel2804@gmail.com>
2025-01-21 14:36:29 +0100

    add DButils

---
+++ src/main/java/DBUtils.java
@@ -0,0 +1,8 @@
+public class DBUtils {
+    private static final String DB_URL = "";
+
+    private static final String DB = "";
+    private static final String DB_USER = "";
+    private static final String DB_PASSWORD = "";
+
+}
